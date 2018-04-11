c47ebd7c30edb5613b4c664119c3f3cd7c85fd61
aabbas90 <ahmed.a.mirza@hotmail.com>
2018-04-11 14:42:17 +0200

    UAI file reading with max potential.

--- include/horizon_tracking_constructor.hxx
+++ include/horizon_tracking_constructor.hxx
@@ -2,52 +2,235 @@
 #define LP_MP_HORIZON_TRACKING_CONSTRUCTOR_HXX
 
 #include "vector.hxx"
+#include "parse_rules.h"
+#include "pegtl/parse.hh"
+#include "tree_decomposition.hxx"
+#include "arboricity.h"
+// #include "mrf_problem_construction.hxx"
+#include <string>
 
 namespace LP_MP {
 
-template<typename MRF_CONSTRUCTOR, typename MAX_FACTOR, typename PAIRWISE_MAX_FACTOR_MESSAGE>
-class horizon_tracking_constructor : public MRF_CONSTRUCTOR {
-public:
-using mrf_constructor = MRF_CONSTRUCTOR;
-using max_factor_container = MAX_FACTOR;
-using pairwise_max_factor_message_container = PAIRWISE_MAX_FACTOR_MESSAGE;
+// template<typename MRF_CONSTRUCTOR, typename MAX_FACTOR, typename PAIRWISE_MAX_FACTOR_MESSAGE>
+// class horizon_tracking_constructor : public MRF_CONSTRUCTOR {
+// public:
+// using mrf_constructor = MRF_CONSTRUCTOR;
+// using max_factor_container = MAX_FACTOR;
+// using pairwise_max_factor_message_container = PAIRWISE_MAX_FACTOR_MESSAGE;
 
-using mrf_constructor::mrf_constructor;
+// using mrf_constructor::mrf_constructor;
 
-template<typename ITERATOR>
-max_factor_container* add_max_factor_on_pairwise(ITERATOR pairwise_begin, ITERATOR pairwise_end, LP_tree* t)
-{
-    const INDEX no_max_vars = std::distance(pairwise_begin, pairwise_end);
-    vector<INDEX> no_labels(no_vars);
-    for(auto it = pairwise_begin; it!=pairwise_end; ++it) {
-        const INDEX i = (*it)[0];
-        const INDEX i = (*it)[1];
-        assert(i<j);
-        no_labels[i] = this->GetNumberOfLabels(i)*this->GetNumberOfLabels(j);
-    }
-    auto* f = new max_factor_container(no_labels.begin(), no_labels.end());
-    this->lp_->AddFactor(f);
+// template<typename ITERATOR>
+// max_factor_container* add_max_factor_on_pairwise(ITERATOR pairwise_begin, ITERATOR pairwise_end, LP_tree* t)
+// {
+//     const INDEX no_max_vars = std::distance(pairwise_begin, pairwise_end);
+//     vector<INDEX> no_labels(no_vars);
+//     for(auto it = pairwise_begin; it!=pairwise_end; ++it) {
+//         const INDEX i = (*it)[0];
+//         const INDEX i = (*it)[1];
+//         assert(i<j);
+//         no_labels[i] = this->GetNumberOfLabels(i)*this->GetNumberOfLabels(j);
+//     }
+//     auto* f = new max_factor_container(no_labels.begin(), no_labels.end());
+//     this->lp_->AddFactor(f);
 
-    INDEX c=0;
-    for(auto it = pairwise_begin; it!=pairwise_end; ++it) {
-        const INDEX i = (*it)[0];
-        const INDEX i = (*it)[1];
-        auto* p = this->GetPairwiseFactor(i,j);
-        auto* m = new pairwise_max_Factor_message_container(c++, p, f);
-        this->lp_->AddMessage(m);
+//     INDEX c=0;
+//     for(auto it = pairwise_begin; it!=pairwise_end; ++it) {
+//         const INDEX i = (*it)[0];
+//         const INDEX i = (*it)[1];
+//         auto* p = this->GetPairwiseFactor(i,j);
+//         auto* m = new pairwise_max_Factor_message_container(c++, p, f);
+//         this->lp_->AddMessage(m);
 
-        if(t != nullptr) {
-            t->AddMessage(m);
-        }
-    } 
+//         if(t != nullptr) {
+//             t->AddMessage(m);
+//         }
+//     } 
 
-    return f;
+//     return f;
+// }
+// };
+
+namespace UAIMaxPotInput {
+
+   struct MaxPotInput {
+      INDEX number_of_variables_;
+      INDEX number_of_cliques_;
+      std::vector<INDEX> cardinality_;
+      std::vector<std::vector<INDEX>> clique_scopes_;
+      std::vector<std::vector<REAL>> function_tables_;
+   };
+
+   // import basic parsers
+   using Parsing::opt_whitespace;
+   using Parsing::mand_whitespace;
+   using Parsing::opt_invisible;
+   using Parsing::mand_invisible;
+   using Parsing::positive_integer;
+   using Parsing::real_number;
+   
+   struct init_line_markov : pegtl::seq< opt_whitespace, pegtl::string<'M','A','R','K','O','V'>, opt_whitespace > {};
+   struct init_line_max_pot : pegtl::seq< opt_whitespace, pegtl::string<'M','A','X','-','P','O','T','E','N','T','I','A','L','S'>, opt_whitespace > {};
+   struct number_of_variables : pegtl::seq< opt_whitespace, positive_integer, opt_whitespace > {};
+   // vector of integers denoting how many labels each variable has
+   struct cardinality : pegtl::seq< opt_whitespace, positive_integer, opt_whitespace > {};
+   struct number_of_cliques : pegtl::seq< opt_whitespace, positive_integer, opt_whitespace> {};
+   // first is the number of variables in the clique, then the actual variables.
+   // the clique_scopes should match number_of_clique_lines, each line consisting of a sequence of integers
+   struct new_clique_scope : pegtl::seq< positive_integer > {};
+   struct clique_scope : pegtl::seq< positive_integer > {};
+   struct clique_scope_line : pegtl::seq< opt_whitespace, new_clique_scope, pegtl::plus< opt_whitespace, clique_scope >, opt_whitespace, pegtl::eol > {};
+   struct clique_scopes_end
+   {
+      //template< pegtl::apply_mode A, pegtl::rewind_mode M, template< typename ... > class Action, template< typename ... > class Control, typename Input >
+      template< pegtl::apply_mode A, template< typename ... > class Action, template< typename ... > class Control, typename Input >
+         static bool match( Input &, std::vector<MaxPotInput>& input )
+         {
+            return input.back().number_of_cliques_ == input.back().clique_scopes_.size();
+         }
+   }; 
+   struct clique_scopes : pegtl::until< clique_scopes_end, clique_scope_line > {};
+   // a function table is begun by number of entries and then a list of real numbers. Here we record all the values in the real stack
+   // do zrobienia: treat whitespace
+   struct new_function_table : pegtl::seq< positive_integer > {};
+   struct function_table_entry : pegtl::seq< real_number > {};
+   struct function_tables_end
+   {
+      //template< pegtl::apply_mode A, pegtl::rewind_mode M, template< typename ... > class Action, template< typename ... > class Control, typename Input >
+      template< pegtl::apply_mode A, template< typename ... > class Action, template< typename ... > class Control, typename Input >
+         static bool match( Input &, std::vector<MaxPotInput>& input )
+         {
+            return input.back().number_of_cliques_ == input.back().function_tables_.size();
+         }
+   };
+   struct function_table_end
+   {
+      //template< pegtl::apply_mode A, pegtl::rewind_mode M, template< typename ... > class Action, template< typename ... > class Control, typename Input >
+      template< pegtl::apply_mode A, template< typename ... > class Action, template< typename ... > class Control, typename Input >
+         static bool match( Input &, std::vector<MaxPotInput>& input )
+         {
+            auto& table = input.back().function_tables_.back();
+            if(table.back() + 1 == table.size()) {
+               table.resize(table.size()-1); // remove last entry which holds the end size of the table
+               return true;
+            } else {
+               return false;
+            }
+         }
+   };
+   struct function_table : pegtl::seq< new_function_table, opt_invisible, pegtl::until< function_table_end, opt_invisible, function_table_entry >, opt_invisible > {};
+   struct function_tables : pegtl::seq< opt_invisible, pegtl::until< function_tables_end, function_table >, opt_invisible > {};//,pegtl::seq<pegtl::star<pegtl::sor<mand_whitespace, pegtl::eol>>, real_number, pegtl::plus<pegtl::star<pegtl::sor<mand_whitespace, pegtl::eol>>, real_number>> {};
+   //template<> struct control< function_table > : pegtl::change_state_and_action< function_table, ..., object_action > {};
+
+   struct grammar : pegtl::seq<pegtl::seq<init_line_markov, pegtl::eol,
+                    number_of_variables, pegtl::eol,
+                    pegtl::plus< cardinality >, pegtl::eol,
+                    number_of_cliques, pegtl::eol,
+                    clique_scopes,
+                    opt_invisible,
+                    function_tables>, 
+                    pegtl::plus<
+                    init_line_max_pot, pegtl::eol,
+                    number_of_variables, pegtl::eol,
+                    pegtl::plus< cardinality >, pegtl::eol,
+                    number_of_cliques, pegtl::eol,
+                    clique_scopes,
+                    opt_invisible,
+                    function_tables
+                   >> {};
+
+
+   template< typename Rule >
+      struct action
+      : pegtl::nothing< Rule > {};
+
+   template<> struct action<init_line_max_pot > {
+      template<typename Input>
+      static void apply(const Input& in, std::vector<MaxPotInput>& input) 
+      {
+         input.resize(input.size() + 1);
+      }
+   };
+
+   template<> struct action<init_line_markov > {
+      template<typename Input>
+      static void apply(const Input& in, std::vector<MaxPotInput>& input) 
+      {
+         input.resize(input.size() + 1);
+      }
+   };
+
+   template<> struct action< number_of_variables > {
+      template<typename Input>
+      static void apply(const Input& in, std::vector<MaxPotInput>& input) 
+      {
+         input.back().number_of_variables_ = std::stoul(in.string());
+      }
+   };
+
+   template<> struct action< number_of_cliques > {
+      template<typename Input>
+      static void apply(const Input & in, std::vector<MaxPotInput>& input)
+      {
+         input.back().number_of_cliques_ = std::stoul(in.string()); 
+      }
+   };
+
+   template<> struct action< cardinality > {
+      template<typename Input>
+      static void apply(const Input & in, std::vector<MaxPotInput>& input)
+      {
+         input.back().cardinality_.push_back(std::stoul(in.string()));
+      }
+   };
+
+   template<> struct action< new_clique_scope > {
+      template<typename Input>
+      static void apply(const Input &, std::vector<MaxPotInput>& input)
+      {
+         input.back().clique_scopes_.push_back(std::vector<INDEX>(0));
+      }
+   };
+   template<> struct action< clique_scope > {
+      template<typename Input>
+      static void apply(const Input & in, std::vector<MaxPotInput>& input)
+      {
+         input.back().clique_scopes_.back().push_back(std::stoul(in.string()));
+         assert(input.back().clique_scopes_.back().back() < input.back().number_of_variables_);
+      }
+   };
+   template<> struct action< new_function_table > {
+      template<typename Input>
+      static void apply(const Input & in, std::vector<MaxPotInput>& input)
+      {
+         const INDEX no_entries = std::stoul(in.string());
+         std::vector<REAL> entries;
+         entries.reserve(no_entries+1);
+         entries.push_back(no_entries);
+         input.back().function_tables_.push_back(std::move(entries));
+      }
+   };
+   template<> struct action< function_table_entry > {
+      template<typename Input>
+      static void apply(const Input & in, std::vector<MaxPotInput>& input)
+      {
+         auto& table = input.back().function_tables_.back();
+         table.push_back(std::stod(in.string()));
+         std::swap(table.back(), *(table.rbegin()+1)); // exchange last element, which always denotes the final number of entries in the function table
+      }
+   };
+
+   bool ParseProblem(const std::string& filename)
+   {
+      // LP_MP::UaiMrfInput::ParseProblem(filename, solver?);
+      std::cout << "parsing " << filename << "\n";
+      pegtl::file_parser problem(filename);
+      std::vector<MaxPotInput> input;
+      bool read_suc = problem.parse< grammar, action >(input);
+      return read_suc;
+   }
+}
 }
 
-
-
-};
-
-}
 
 #endif // LP_MP_HORIZON_TRACKING_CONSTRUCTOR_HXX
