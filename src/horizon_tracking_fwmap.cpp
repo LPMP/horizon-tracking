44e8ef0238bdd7da353736f5092838ce7585860e
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2018-07-27 00:13:23 +0200

    cout changes.

--- src/horizon_tracking_fwmap.cpp
+++ src/horizon_tracking_fwmap.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
     solver.GetLP().ComputePassAndPrimal<std::vector<FactorTypeAdapter*>::iterator, Direction::backward>(factors.begin(), factors.end(), 2);
     solver.RegisterPrimal();
     solver.WritePrimal();
-    std::cout<<"\n Primal Cost: "<<solver.primal_cost();
-    std::cout<<"Percentage of primal cost to lower bound: "<<100.0 * (solver.primal_cost() - solver.lower_bound()) / solver.lower_bound() <<"%";
+    std::cout<<"\n\n Primal Cost: "<<solver.primal_cost();
+    std::cout<<"\n Percentage duality gap: "<<100.0 * (solver.primal_cost() - solver.lower_bound()) / solver.lower_bound() <<"%\n\n";
 }
  
