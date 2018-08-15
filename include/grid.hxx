45a1856180fce55aac917d517c1ff38b3dd1a18e
Ahmed Abbas <ahmed.a.mirza@hotmail.com>
2018-08-15 16:37:20 +0200

    Fixed grid arragement

--- include/grid.hxx
+++ include/grid.hxx
@@ -15,8 +15,8 @@ namespace LP_MP {
         std::array<std::size_t,2> node_to_grid(const std::size_t i) const 
         {
             assert(i < size_x*size_y);
-            const std::size_t x = i/size_y;
-            const std::size_t y = i%size_y;
+            const std::size_t x = i%size_x;
+            const std::size_t y = i/size_x;
             return {x,y}; 
         }
 
@@ -26,18 +26,18 @@ namespace LP_MP {
             if(size_x == 1 || size_y == 1) {
                 assert(node_2 == node_1 + 1 && node_2 < size_x*size_y);
                 return {0,node_1}; 
-            } else if(node_2 == node_1 + 1) {
-                const auto coord_1 = node_to_grid(node_1);
-                const auto coord_2 = node_to_grid(node_2);
-                assert(coord_2[1] == coord_1[1] + 1);
-                assert(coord_2[0] == coord_1[0]);
-                return {coord_1[0], coord_1[1]};
-            } else if(node_2 == node_1 + size_x) {
+            } else if(node_2 == node_1 + 1) { // Horizontally arranged
                 const auto coord_1 = node_to_grid(node_1);
                 const auto coord_2 = node_to_grid(node_2);
                 assert(coord_2[0] == coord_1[0] + 1);
-                assert(coord_1[1] == coord_2[1]);
-                return {coord_1[1] + size_x, coord_1[0]}; 
+                assert(coord_2[1] == coord_1[1]);
+                return {coord_1[1], coord_1[0]};
+            } else if(node_2 == node_1 + size_x) { // Vertically arranged
+                const auto coord_1 = node_to_grid(node_1);
+                const auto coord_2 = node_to_grid(node_2);
+                assert(coord_2[1] == coord_1[1] + 1);
+                assert(coord_1[0] == coord_2[0]);
+                return {coord_1[0] + size_y, coord_1[1]}; 
             } else {
                 throw std::runtime_error("nodes must be neighboring in grid graph");
             }
@@ -57,29 +57,29 @@ namespace LP_MP {
                 std::iota(c.begin(), c.end(), 0);
                 return c;
             } else {
-                if(chain_no < size_x) { return horizontal_chain(chain_no); }
-                else { return vertical_chain(chain_no - size_x); }
+                if(chain_no < size_y) { return horizontal_chain(chain_no); }
+                else { return vertical_chain(chain_no - size_y); }
             }
         }
 
-        std::vector<std::size_t> horizontal_chain(const std::size_t i) const 
-        {
-            assert(i < size_x);
-            std::vector<std::size_t> nodes;
-            nodes.reserve(size_y);
-            for(std::size_t j=0; j<size_y; j++) {
-                nodes.push_back(i*size_y + j);
-            }
-            return nodes;
-        }
-
-        std::vector<std::size_t> vertical_chain(const std::size_t j) const 
+        std::vector<std::size_t> horizontal_chain(const std::size_t j) const 
         {
             assert(j < size_y);
             std::vector<std::size_t> nodes;
             nodes.reserve(size_x);
             for(std::size_t i=0; i<size_x; i++) {
-                nodes.push_back(i*size_y + j);
+                nodes.push_back(j*size_x + i);
+            }
+            return nodes;
+        }
+
+        std::vector<std::size_t> vertical_chain(const std::size_t i) const 
+        {
+            assert(i < size_x);
+            std::vector<std::size_t> nodes;
+            nodes.reserve(size_y);
+            for(std::size_t j=0; j<size_y; j++) {
+                nodes.push_back(j*size_x + i);
             }
             return nodes;
         }
